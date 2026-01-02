63863aa59459b8b9e9691f877455711116bb19b1
z-index <yg_zinger@163.com>
2026-01-02 19:15:11 +0800

    fix: update EMAIL constant to correct address

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,4 @@ export const SITE_TITLE = 'z-index.me'
 export const SITE_DESCRIPTION
   = 'z-index.me is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.'
 
-export const EMAIL = 'hello@nicholasly.com'
+export const EMAIL = 'yg_zinger@163.com'
