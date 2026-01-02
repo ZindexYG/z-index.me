c6cf30984252cb1aac8bb72727518db30b3341a4
z-index <yg_zinger@163.com>
2026-01-03 01:41:28 +0800

    fix: update SITE_TITLE and SITE_DESCRIPTION for accuracy and localization

--- src/consts.ts
+++ src/consts.ts
@@ -2,8 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_URL = 'https://z-index.me'
-export const SITE_TITLE = 'z-index.me'
+export const SITE_TITLE = "YG's blog"
 export const SITE_DESCRIPTION
-  = 'z-index.me is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.'
+  = '时针滴答一声，响了'
 
 export const EMAIL = 'yg_zinger@163.com'
