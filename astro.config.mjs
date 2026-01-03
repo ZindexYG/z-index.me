23140514bd10ee8b1635fa564b8b899ac44ff1b2
z-index <yg_zinger@163.com>
2026-01-03 18:25:43 +0800

    refactor: integrate pagefind for enhanced search functionality and update styles for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import pagefind from "astro-pagefind";
 
 import { defineConfig } from 'astro/config'
 
 import { SITE_URL } from './src/consts'
 
+
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), pagefind()],
   markdown: {
     shikiConfig: {
       themes: {
