6fdcb3e9bbca1ce0df1e9b70ee32031dde5e8a67
z-index <yg_zinger@163.com>
2026-01-03 21:15:17 +0800

    feat: enhance markdown processing with additional remark plugins and update integration settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,39 @@ import { defineConfig } from 'astro/config'
 
 import { SITE_URL } from './src/consts'
 
+import remarkFigureCaption from '@microflash/remark-figure-caption';
+import remarkDirective from 'remark-directive';
+import remarkCalloutDirectives from "./src/components/mdrenders/remark-callout-directives-customized.mjs"
+import remarkNeoDB from "./src/components/mdrenders/remark-neodb-card.mjs"
+import { remarkModifiedTime } from './src/components/mdrenders/remark-modified-time.mjs';
 
+import astroExpressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap(), pagefind()],
+  integrations: [
+    astroExpressiveCode({
+      // Replace the default themes with a custom set of bundled themes:
+      // "dracula" (a dark theme) and "solarized-light"
+      themes: ['andromeeda', 'dracula'],
+    }),
+    sitemap(),
+    pagefind()
+  ],
   markdown: {
-    shikiConfig: {
-      themes: {
-        light: 'catppuccin-latte',
-        dark: 'catppuccin-mocha',
-      },
-    },
+    // shikiConfig: {
+    //   themes: {
+    //     light: 'catppuccin-latte',
+    //     dark: 'catppuccin-mocha',
+    //   },
+    // },
+    remarkPlugins: [
+      remarkFigureCaption,
+      remarkNeoDB,
+      remarkDirective,
+      remarkCalloutDirectives,
+      remarkModifiedTime
+    ],
   },
 })
